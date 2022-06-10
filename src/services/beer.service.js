27c924beaac0cd246b416248bd61ad5b92aefc2a
aimeejenner <aimeejenner@gmail.com>
2022-06-11 00:02:11 +0100

    Added index.html file

--- src/services/beer.service.js
+++ src/services/beer.service.js
@@ -3,7 +3,7 @@ const API_URL = "https://api.punkapi.com/v2/beers";
 
 /**
  * GET - Returns beers for a given search term.
- *       NOTE: We can leave this untested as it would ONLY test the punk API, which is not our responsibility
+ *  
  * @param {string} searchTerm
  */
  const getBeers = (searchTerm) => {
@@ -16,15 +16,8 @@ const API_URL = "https://api.punkapi.com/v2/beers";
 
 export default getBeers;
 
-//   useEffect(() => {
-//     fetch("https://api.punkapi.com/v2/beers?per_page=80")
-//       .then((res) => res.json())
-//       .then((jsonResponse) => {
-//         setBeers(jsonResponse);
-//       });
-// }, []);
 
 
 // high alcohol - https://api.punkapi.com/v2/beers?abv_gt=6
 // classic range - https://api.punkapi.com/v2/beers?brewed_before=01-2010
-// high acidity - getBeers then return beers where .ph property < 4
+// high acidity - return beers where .ph property < 4
--- src/services/dist/beer.service.dev.js
+++ src/services/dist/beer.service.dev.js
@@ -7,7 +7,7 @@ exports["default"] = void 0;
 var API_URL = "https://api.punkapi.com/v2/beers";
 /**
  * GET - Returns beers for a given search term.
- *       NOTE: We can leave this untested as it would ONLY test the punk API, which is not our responsibility
+ *  
  * @param {string} searchTerm
  */
 
@@ -19,15 +19,8 @@ var getBeers = function getBeers(searchTerm) {
   });
 };
 
-var _default = getBeers; //   useEffect(() => {
-//     fetch("https://api.punkapi.com/v2/beers?per_page=80")
-//       .then((res) => res.json())
-//       .then((jsonResponse) => {
-//         setBeers(jsonResponse);
-//       });
-// }, []);
-// high alcohol - https://api.punkapi.com/v2/beers?abv_gt=6
+var _default = getBeers; // high alcohol - https://api.punkapi.com/v2/beers?abv_gt=6
 // classic range - https://api.punkapi.com/v2/beers?brewed_before=01-2010
-// high acidity - getBeers then return beers where .ph property < 4
+// high acidity - return beers where .ph property < 4
 
 exports["default"] = _default;
